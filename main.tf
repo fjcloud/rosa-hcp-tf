e24a5d78af9074b3e7aa0de74b60af5dfb32ae6c
fjcloud <fj@msl.cloud>
2024-03-11 16:56:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
     }
     rhcs = {
       version = ">= 1.5.1"
-      source  = "terraform-redhat/rhcs"
+      source  = "terraform.local/local/rhcs"
     }
   }
 }
@@ -63,7 +63,7 @@ locals {
   version    = var.openshift_version
 
   vpc_cidr = var.vpc_cidr
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs      = slice(data.aws_availability_zones.available.names, 0, 1)
 }
 
 # Create managed OIDC config
@@ -78,23 +78,6 @@ module "oidc_config" {
   path                 = var.path
 }
 
-module "create_account_roles" {
-  source  = "terraform-redhat/rosa-sts/aws"
-  version = ">=0.0.15"
-
-  create_account_roles = true
-
-  account_role_prefix    = var.account_role_prefix
-  ocm_environment        = var.ocm_environment
-  rosa_openshift_version = join(".", slice(split(".", local.version), 0, 2))
-  account_role_policies  = data.rhcs_policies.all_policies.account_role_policies
-  operator_role_policies = data.rhcs_policies.all_policies.operator_role_policies
-  all_versions           = data.rhcs_versions.all
-  path                   = var.path
-  tags                   = var.tags
-}
-
-
 resource "rhcs_cluster_rosa_hcp" "rosa_hcp_cluster" {
   name                   = local.name
   sts                    = local.sts_roles
@@ -106,16 +89,10 @@ resource "rhcs_cluster_rosa_hcp" "rosa_hcp_cluster" {
   multi_az           = var.multi_az
 
   version = local.version
-  # aws_subnet_ids       = join(",", concat(module.vpc.public_subnets, module.vpc.private_subnets))
+  aws_subnet_ids       = join(",", concat(module.vpc.public_subnets, module.vpc.private_subnets))
   machine_cidr         = local.vpc_cidr
-  worker_disk_size     = 300
   compute_machine_type = "m5.xlarge"
-  # autoscaling_enabled         = true
-  # min_replicas                = 3
-  # max_replicas                = 6
   replicas                    = 3
-  default_mp_labels           = { "MachinePool" = "core" }
-  disable_workload_monitoring = true
 
   properties = {
     rosa_creator_arn = data.aws_caller_identity.current.arn
@@ -143,89 +120,31 @@ data "rhcs_rosa_operator_roles" "operator_roles" {
   account_role_prefix  = var.account_role_prefix
 }
 
-module "operator_roles" {
-  source  = "terraform-redhat/rosa-sts/aws"
-  version = ">=0.0.15"
 
-  create_operator_roles = true
-  create_oidc_provider  = false
+module "vpc" {
+ source  = "terraform-aws-modules/vpc/aws"
+ version = "~> 5.0"
+ 
+ name = local.name
+ cidr = local.vpc_cidr
+ 
+ azs             = local.azs
+ private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+ public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+ 
+ enable_nat_gateway      = true
+ single_nat_gateway      = var.multi_az
+ enable_dns_hostnames    = true
+ enable_dns_support      = true
+ map_public_ip_on_launch = true
+ 
+ public_subnet_tags = {
+ "kubernetes.io/role/elb" = 1
+ }
 
-  cluster_id                  = rhcs_cluster_rosa_hcp.rosa_hcp_cluster.id
-  rh_oidc_provider_thumbprint = rhcs_cluster_rosa_hcp.rosa_hcp_cluster.sts.thumbprint
-  rh_oidc_provider_url        = rhcs_cluster_rosa_hcp.rosa_hcp_cluster.sts.oidc_endpoint_url
-  operator_roles_properties   = data.rhcs_rosa_operator_roles.operator_roles.operator_iam_roles
-  tags                        = var.tags
-}
-
-resource "rhcs_machine_pool" "mp1_machine_pool" {
-  cluster             = rhcs_cluster_rosa_hcp.rosa_hcp_cluster.id
-  name                = "mp1"
-  machine_type        = "r6i.2xlarge"
-  disk_size           = 300
-  autoscaling_enabled = true
-  min_replicas        = 3
-  max_replicas        = 9
-  labels              = { "MachinePool" = "mp1" }
-  # taints = [{
-  # key           = "dedicated",
-  # value         = "mp1",
-  # schedule_type = "NoSchedule"
-  # }]
-
-  depends_on = [rhcs_cluster_rosa_hcp.rosa_hcp_cluster]
-}
-
-
-#---------------------------------------------------------------
-# Cluster Admin credentials resources
-#---------------------------------------------------------------
-data "aws_secretsmanager_secret_version" "admin_password_version" {
-  secret_id  = aws_secretsmanager_secret.rosa_hcp.id
-  depends_on = [aws_secretsmanager_secret_version.rosa_hcp]
-}
-
-resource "random_password" "rosa_hcp" {
-  length           = 16
-  special          = true
-  override_special = "@_"
-}
-
-#tfsec:ignore:aws-ssm-secret-use-customer-key
-resource "aws_secretsmanager_secret" "rosa_hcp" {
-  name                    = local.name
-  recovery_window_in_days = 0 # Set to zero for this example to force delete during Terraform destroy
-}
-
-resource "aws_secretsmanager_secret_version" "rosa_hcp" {
-  secret_id     = aws_secretsmanager_secret.rosa_hcp.id
-  secret_string = random_password.rosa_hcp.result
-}
-
-
-# module "vpc" {
-# source  = "terraform-aws-modules/vpc/aws"
-# version = "~> 5.0"
-# 
-# name = local.name
-# cidr = local.vpc_cidr
-# 
-# azs             = local.azs
-# private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-# public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
-# 
-# enable_nat_gateway      = true
-# single_nat_gateway      = var.multi_az
-# enable_dns_hostnames    = true
-# enable_dns_support      = true
-# map_public_ip_on_launch = true
-# 
-# public_subnet_tags = {
-# "kubernetes.io/role/elb" = 1
-# }
-# 
-# private_subnet_tags = {
-# "kubernetes.io/role/internal-elb" = 1
-# }
-# 
-# tags = var.tags
-# }
+ private_subnet_tags = {
+ "kubernetes.io/role/internal-elb" = 1
+ }
+ 
+ tags = var.tags
+ }
